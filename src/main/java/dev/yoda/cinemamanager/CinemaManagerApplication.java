ab3de9f7f47b5abb8fcbd585db8aaae810654519
YodaBikarbona <mihael.peric@hotmail.com>
2024-01-10 15:59:23 +0100

    removed unused stuff

--- src/main/java/dev/yoda/cinemamanager/CinemaManagerApplication.java
+++ src/main/java/dev/yoda/cinemamanager/CinemaManagerApplication.java
@@ -1,15 +1,8 @@
 package dev.yoda.cinemamanager;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// @EnableAutoConfiguration
-// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-// @EnableJpaRepositories(basePackages = "dev.yoda.cinemamanager.repository")
 @SpringBootApplication
 public class CinemaManagerApplication {
 
